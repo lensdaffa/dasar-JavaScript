eae03decbfc75de002e536f24bfbe8bbf9c8f6b2
Aydin Akmal Daffa <lensdaffa@gmail.com>
2026-01-12 21:27:06 +0700

    Add files via upload

---
+++ tipe data/script.3.js
@@ -0,0 +1,24 @@
+// tipe data
+
+let dataString = "aydin" // string
+console.log(dataString)
+console.log(typeof dataString)
+
+let dataNumber = 12.209 // number
+console.log(dataNumber)
+console.log(typeof dataNumber)
+
+let dataBoolean = false // boolean
+console.log(dataBoolean)
+console.log(typeof dataBoolean)
+
+dataString = 2
+console.log(dataString)
+console.log(typeof dataString)
+
+let dataKosong // undefined
+console.log(dataKosong)
+console.log(typeof dataKosong)
+dataKosong = "p"
+console.log(dataKosong)
+console.log(typeof dataKosong)
