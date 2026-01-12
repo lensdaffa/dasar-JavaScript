101f443ef215781e62a8e37c43454dcca428cde0
Aydin Akmal Daffa <lensdaffa@gmail.com>
2026-01-12 21:51:58 +0700

    Add files via upload

---
+++ if statement (percabangan)/script15.js
@@ -0,0 +1,11 @@
+let nama = "otong"
+
+console.log("nama" + " = " + nama)
+
+if (nama==="otong"){
+    console.log("halooooooo")
+}else{
+    console.log("haiiiiiiiiiiiiii")
+}
+
+console.log("akhir dari program")
