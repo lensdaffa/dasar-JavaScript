ffb9dbe0ef48c4054ca0fc1322b6a6642f127684
Aydin Akmal Daffa <lensdaffa@gmail.com>
2026-01-12 21:20:39 +0700

    Add files via upload

---
+++ variabel/script2.js
@@ -0,0 +1,44 @@
+// 1. variabel dengan let
+let nama = "aydin";
+
+//tampilkan data
+console.log(nama);
+
+//ubah nilai variabel nama
+nama = "daffa";
+console.log(nama);
+
+// 2. variabel dengan var
+var namaDepan = "aydin";
+console.log(namaDepan); 
+namaDepan = "aku aydin";
+console.log(namaDepan);
+ 
+// kelakukan dari let
+let namaBelakang = "daffa"
+{
+    let namaBelakang = "daffa nih"
+    console.log(namaBelakang)
+}
+console.log(namaBelakang)
+
+//kelakukan dari var
+var namaTengah = "akmal"
+{
+    var namaTengah = "ini akmal"
+    console.log(namaTengah)
+}
+console.log(namaTengah)
+
+//tanpa keyword
+gorengan = "bala - bala"
+{
+    gorengan = "combro"
+}
+console.log(gorengan) 
+
+// 3. const
+
+const TTL = "7 Desember 1997"
+console.log(TTL)
+// TTL = "11 maret 2012"; tidak bisa dilakukan karena sudah konstan
