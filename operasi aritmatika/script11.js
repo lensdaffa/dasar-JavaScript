540993a5c0f9116f66fefdaf86d662c556d01b28
Aydin Akmal Daffa <lensdaffa@gmail.com>
2026-01-12 21:47:15 +0700

    Add files via upload

---
+++ operasi aritmatika/script11.js
@@ -0,0 +1,37 @@
+// ekspresi dan statment
+
+// */+-
+// % <- modulo
+// ++ <- increment
+// -- <- decrement
+// ** <- pangkat atau power
+
+let x = 10
+console.log(x + 1)
+console.log(x - 1)
+console.log(x / 4)
+console.log(x * 2)
+
+//aritmatika khusus di JavaScript
+console.log(x % 3)
+console.log(x ** 2)
+
+//pemrograman biasa
+x = x + 1
+console.log(x)
+x = x + 1
+console.log(x)
+x++
+x++
+console.log(x)
+
+x = x - 1
+console.log(x)
+x--
+console.log(x)
+
+//studi kasus
+let y = 1 + "1"
+console.log(y) //string
+y = 1 + parseInt("1")
+console.log(y)
