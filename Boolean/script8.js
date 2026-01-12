fbcb78471302fc87b961d9147ff1c3636b5835bc
Aydin Akmal Daffa <lensdaffa@gmail.com>
2026-01-12 21:43:53 +0700

    Add files via upload

---
+++ Boolean/script8.js
@@ -0,0 +1,19 @@
+// tipe data boolean
+
+let data_bool_1 = false
+console.log(data_bool_1)
+
+let data_bool_2 = (3<10)
+console.log(data_bool_2)
+
+let data_bool_3 = ("aydin" == "akmal")
+console.log(data_bool_3)
+
+let data_bool_4 = (false == 0)
+console.log(data_bool_4)
+
+data_bool_4 = (false == 0n)
+console.log(data_bool_4)
+
+data_bool_4 = (false == " ")
+console.log(data_bool_4)
